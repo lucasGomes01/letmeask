15e96ed0111830893225dd3954c0361e49cc2315
lucasGomes01 <71887255+lucasGomes01@users.noreply.github.com>
2021-06-22 14:55:17 -0300

    final aula2

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,26 +1,16 @@
+// import { useContext } from 'react';
+import { Link } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg'
-
-import { useHistory } from 'react-router-dom';
 
 import { Button } from '../components/Button';
 
-
 import '../styles/auth.scss'
-import { useAuth } from '../hooks/useAuth';
+// import { useAuth } from '../hooks/useAuth';
 
-export function Home(){
-  const history = useHistory();
-  const { user, signInWithGoogle } = useAuth()
-
-  async function handleCreateRoom() {
-    if (!user) {
-      await  signInWithGoogle()
-    }
- 
-    history.push('/rooms/new');
-  }
+export function NewRoom (){
+  // const { user } = useAuth()
 
   return (
     <div id="page-auth">
@@ -32,20 +22,19 @@ export function Home(){
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button onClick={handleCreateRoom} className="create-room">
-            <img src={googleIconImg} alt="Logo do Google" />
-            Crie uma sala com o google
-          </button>
-          <div className="separator">ou entre em uma sala</div>
+         <h2>Cria nova sala</h2>
           <form action="">
             <input 
               type="text"
-              placeholder="Digite o  código da sala"
+              placeholder="Nome da sala"
             />
             <Button type="submit">
-              Entrar na sala
+              Criar sala
             </Button>
           </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+          </p>
         </div>
       </main>
     </div>
